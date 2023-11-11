fcb19d9099e9d1f2d8b8cf84caf1d4568842d1e5
NEERAJ023 <143694023+NEERAJ023@users.noreply.github.com>
2023-11-11 16:51:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let a=prompt("enter a value");
+let b=prompt("enter second value");
+
+let c=parseInt(a);
+c=c*2;
+let d=parseInt(b)
+let result=c+d;
+alert("The final result is: "+result);
